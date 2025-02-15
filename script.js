da5a7d549cad44573bb8f119aabd568446be74bf
Cristian Toni <cristian.bengardini@gmail.com>
2025-02-14 21:10:17 -0300

    API para horario, y media queries

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ import API_KEY from "./apiKey.js";
 const urlBase = `https://www.meteosource.com/api/v1/free`;
 
 
-// const container = document.querySelector('.container');
 const sectionToday = document.querySelector('.today');
 const title = document.querySelector('.title');
 const time = document.querySelector('.time');
 const divToday = document.querySelector('.today-weather');
 const sectionDaily = document.querySelector('.daily');
 const titleDaily = document.querySelector('.title-daily');
-// const sectionHourly = document.querySelector('.hourly');
-// const titleHourly = document.querySelector('.title-hourly');
 const swiffySlider = document.querySelector('.swiffy-slider');
 const sliderContainer = document.querySelector('.slider-container');
 sectionToday.style.display = 'none';
@@ -25,27 +22,36 @@ document.getElementById('searchButton').addEventListener('click', () => {
     const fomattedCity = city.join('-');
     if (fomattedCity) {
         fetchWeather(fomattedCity);
-    } else {
-        alert('Ingrese una ciudad válida');
     }
 });
 
 function fetchWeather(city) {
     fetch(`${urlBase}/point?place_id=${city}&sections=all&timezone=auto&language=en&units=ca&key=${API_KEY}`)
-        .then(data => data.json())
         .then(data => {
-            console.log(data)
-            showWeatherData(data)
+            if (data.status === 200) {
+                return data.json()
+            }
+        }).then(data => {
+            showWeatherData(data);
         })
+        .catch(error => {
+            sectionToday.style.display = 'none';
+            Swal.fire({
+                position: "top-end",
+                icon: "error",
+                title: "Debe ingresar una ciudad",
+                showConfirmButton: false,
+                timer: 1500
+            });
+            console.error(error)
+        });
 }
-
 
 function showWeatherData(data) {
     const currentParagraph = document.querySelector('.text');
     const currentIcon = document.querySelector('.current_icon');
     const currentTemp = document.querySelector('.temp');
     const currentWind = document.querySelector('.wind');
-    const date = new Date()
     const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
     const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
@@ -53,14 +59,26 @@ function showWeatherData(data) {
     sectionToday.style.display = 'block';
     divToday.style.display = 'flex';
     let cityName = document.getElementById('cityInput').value;
-    let currentDay = days[date.getDay()]
-    let hours = `${date.getHours()}:${date.getMinutes().toLocaleString('es-ES')}`
+    let numberDay = '';
+    let currentDay = '';
+    let hours = '';
+
+    let zone = data.timezone.split('/');
+    fetch(`https://timeapi.io/api/time/current/zone?timeZone=${zone.join('%2F')}`)
+        .then(data => data.json())
+        .then(data => {
+            numberDay = new Date(data.date).getDay();
+            currentDay = days[numberDay];
+            hours = data.time;
+            time.textContent = `${hours} | ${currentDay}`;
+        })
+        .catch(error => console.error(error));
+
     title.textContent = `Ahora en ${cityName.toLowerCase().replace(/\b\w/g, match => match.toUpperCase())}`
-    time.textContent = `${hours} | ${currentDay}`;
     currentParagraph.textContent = data.current.summary;
     currentIcon.src = `./assets/img/big/${data.current.icon_num}.png`;
     currentTemp.textContent = `${data.current.temperature}°`;
-    currentWind.innerHTML = `<p><img src='./assets/img/icons8-viento-48.png' /> ${data.current.wind.speed}Km/h</p>`;
+    currentWind.innerHTML = `<img src='./assets/img/icons8-viento-48.png' /><p>${data.current.wind.speed}Km/h</p>`;
 
     // Hourly
     swiffySlider.style.display = 'block';
@@ -70,7 +88,7 @@ function showWeatherData(data) {
         divHour.innerHTML = `
             <p>${hour.date.slice(11)}</p>
             <img class='icon-hour' src='./assets/img/small/${hour.icon}.png' alt='icono de clima' />
-            <p>${hour.temperature}</p>
+            <p>${hour.temperature}°</p>
             <div><img src='./assets/img/icons8-viento-30.png' /><p>${hour.wind.speed}Km/h<p/></div>`
 
         sliderContainer.append(divHour);
